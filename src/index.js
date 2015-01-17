1de2aaa5df6d0495ac28bff38fa3c0f679207d6c
Phil Booth <pmbooth@gmail.com>
2015-01-17 22:23:17 +0000

    Index module.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*globals module */
+
+'use strict';
+
+module.exports = {
+    parse: require('./parse'),
+    serialise: require('./serialise'),
+    read: require('./read'),
+    write: require('./write')
+};
+
