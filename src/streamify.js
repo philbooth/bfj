5531578cd7bea3c466210cb52acef9252986fc4e
Phil Booth <pmbooth@gmail.com>
2015-04-27 22:47:06 +0100

    More streamify implementation.

--- src/streamify.js
+++ src/streamify.js
@@ -7,6 +7,8 @@ var eventify, events;
 eventify = require('./eventify');
 events = require('./events');
 
+module.exports = streamify;
+
 /**
  * Public function `streamify`.
  *
@@ -27,9 +29,94 @@ events = require('./events');
  **/
 
 function streamify (data, options) {
-    var stream, emitter;
+    var stream, emitter, json, ended;
 
-    stream = /* TODO: Instantiate readable stream */;
+    // TODO: options.buffers, options.replacer, options.space
+
+    stream = new JsonStream(push);
     emitter = eventify(data, options);
+    json = '';
+
+    emitter.on(events.array, array);
+    emitter.on(events.object, object);
+    emitter.on(events.property, property);
+    emitter.on(events.string, string);
+    emitter.on(events.number, value);
+    emitter.on(events.literal, value);
+    emitter.on(events.endArray, endArray);
+    emitter.on(events.endObject, endObject);
+    emitter.on(events.end, end);
+
+    return stream;
+
+    function push () {
+        if (ended) {
+            return stream.push(null);
+        }
+
+        stream.push(json, 'utf8');
+        json = '';
+    }
+
+    // TODO: Work out how to do commas sanely
+
+    function array () {
+        json += '[';
+    }
+
+    function object () {
+        json += '{';
+    }
+
+    function property (name) {
+        json += '"' + name + '":';
+    }
+
+    function string (s) {
+        value('"' + s + '"');
+    }
+
+    function value (v) {
+        json += v;
+    }
+
+    function endArray () {
+        json += ']';
+    }
+
+    function endObject () {
+        json += '}';
+    }
+
+    function end () {
+        ended = true;
+    }
+}
+
+// HACK: What follows is only here temporarily, until #5 is resolved.
+var util, Readable, check;
+
+util = require('util');
+Readable = require('stream').Readable;
+check = require('check-types');
+
+util.inherits(JsonStream, Readable);
+
+function JsonStream (read) {
+    // https://nodejs.org/api/stream.html#stream_class_stream_readable_1
+
+    if (check.not.instance(this, JsonStream)) {
+        return new JsonStream(read);
+    }
+
+    check.assert.function(read, 'Invalid read implementation');
+
+    this._read = function () {
+        // TODO: Check it is okay to ignore size argument
+
+        read();
+    };
+
+    return Readable.call(this, { encoding: 'utf8' });
 }
 
