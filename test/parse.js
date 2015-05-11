60486e73430b6260dc2bb0b5f7dce4e26dfedbed
Phil Booth <pmbooth@gmail.com>
2015-05-11 23:21:16 +0100

    Unit tests for reviver option.

--- test/parse.js
+++ test/parse.js
@@ -9,6 +9,7 @@ spooks = require('spooks');
 modulePath = '../src/parse';
 
 mockery.registerAllowable(modulePath);
+mockery.registerAllowable('check-types');
 mockery.registerAllowable('./events');
 
 suite('parse:', function () {
@@ -65,6 +66,18 @@ suite('parse:', function () {
         test('parse does not throw', function () {
             assert.doesNotThrow(function () {
                 parse();
+            });
+        });
+
+        test('parse throws if reviver is an object', function () {
+            assert.throws(function () {
+                parse({}, { reviver: {} });
+            });
+        });
+
+        test('parse does not throw if revive is a function', function () {
+            assert.doesNotThrow(function () {
+                parse({}, { reviver: function () {} });
             });
         });
 
@@ -625,6 +638,176 @@ suite('parse:', function () {
                     test('resolve was called correctly', function () {
                         assert.isObject(log.args.resolve[0][0]);
                         assert.lengthOf(Object.keys(log.args.resolve[0][0]), 0);
+                    });
+                });
+            });
+        });
+
+        suite('parse with reviver:', function () {
+            var stream, options;
+
+            setup(function () {
+                stream = {};
+                options = { reviver: spooks.fn({ name: 'reviver', log: log, results: [ 'reviver result' ] }) };
+                parse(stream, options)
+                    .then(spooks.fn({ name: 'resolve', log: log }))
+                    .catch(spooks.fn({ name: 'reject', log: log }));
+            });
+
+            teardown(function () {
+                stream = options = undefined;
+            });
+
+            test('walk was called once', function () {
+                assert.strictEqual(log.counts.walk, 1);
+            });
+
+            test('EventEmitter.on was called ten times', function () {
+                assert.strictEqual(log.counts.on, 10);
+            });
+
+            suite('populated array events:', function () {
+                setup(function () {
+                    log.args.on[0][1]();
+                    log.args.on[3][1]('foo');
+                    log.args.on[3][1]('bar');
+                    log.args.on[0][1]();
+                    log.args.on[0][1]();
+                    log.args.on[3][1]('baz');
+                    log.args.on[6][1]();
+                    log.args.on[3][1]('qux');
+                    log.args.on[6][1]();
+                    log.args.on[6][1]();
+                });
+
+                test('resolve was not called', function () {
+                    assert.strictEqual(log.counts.resolve, 0);
+                });
+
+                test('reviver was not called', function () {
+                    assert.strictEqual(log.counts.resolve, 0);
+                });
+
+                suite('end event:', function () {
+                    setup(function (done) {
+                        log.args.on[8][1]();
+                        setImmediate(done);
+                    });
+
+                    test('resolve was called once', function () {
+                        assert.strictEqual(log.counts.resolve, 1);
+                    });
+
+                    test('reviver was called six times', function () {
+                        assert.strictEqual(log.counts.reviver, 7);
+                    });
+
+                    test('reviver was called correctly first time', function () {
+                        assert.lengthOf(log.args.reviver[0], 2);
+                        assert.strictEqual(log.args.reviver[0][0], '0');
+                        assert.strictEqual(log.args.reviver[0][1], 'foo');
+                    });
+
+                    test('reviver was called correctly second time', function () {
+                        assert.strictEqual(log.args.reviver[1][0], '1');
+                        assert.strictEqual(log.args.reviver[1][1], 'bar');
+                    });
+
+                    test('reviver was called correctly third time', function () {
+                        assert.strictEqual(log.args.reviver[2][0], '0');
+                        assert.strictEqual(log.args.reviver[2][1], 'baz');
+                    });
+
+                    test('reviver was called correctly fourth time', function () {
+                        assert.strictEqual(log.args.reviver[3][0], '0');
+                        assert.isArray(log.args.reviver[3][1]);
+                        assert.lengthOf(log.args.reviver[3][1], 1);
+                        assert.strictEqual(log.args.reviver[3][1][0], 'reviver result');
+                    });
+
+                    test('reviver was called correctly fifth time', function () {
+                        assert.strictEqual(log.args.reviver[4][0], '1');
+                        assert.strictEqual(log.args.reviver[4][1], 'qux');
+                    });
+
+                    test('reviver was called correctly sixth time', function () {
+                        assert.strictEqual(log.args.reviver[5][0], '2');
+                        assert.isArray(log.args.reviver[5][1]);
+                        assert.lengthOf(log.args.reviver[5][1], 2);
+                        assert.strictEqual(log.args.reviver[5][1][0], 'reviver result');
+                        assert.strictEqual(log.args.reviver[5][1][1], 'reviver result');
+                    });
+
+                    test('reviver was called correctly seventh time', function () {
+                        assert.strictEqual(log.args.reviver[6][0], '');
+                        assert.isArray(log.args.reviver[6][1]);
+                        assert.lengthOf(log.args.reviver[6][1], 3);
+                        assert.strictEqual(log.args.reviver[6][1][0], 'reviver result');
+                        assert.strictEqual(log.args.reviver[6][1][1], 'reviver result');
+                        assert.strictEqual(log.args.reviver[6][1][2], 'reviver result');
+                    });
+                });
+            });
+
+            suite('populated object events:', function () {
+                setup(function () {
+                    log.args.on[1][1]();
+                    log.args.on[2][1]('foo');
+                    log.args.on[1][1]();
+                    log.args.on[2][1]('bar');
+                    log.args.on[4][1](3.14159265359);
+                    log.args.on[7][1]();
+                    log.args.on[2][1]('baz');
+                    log.args.on[5][1](null);
+                    log.args.on[7][1]();
+                });
+
+                test('resolve was not called', function () {
+                    assert.strictEqual(log.counts.resolve, 0);
+                });
+
+                test('reviver was not called', function () {
+                    assert.strictEqual(log.counts.resolve, 0);
+                });
+
+                suite('end event:', function () {
+                    setup(function (done) {
+                        log.args.on[8][1]();
+                        setImmediate(done);
+                    });
+
+                    test('resolve was called once', function () {
+                        assert.strictEqual(log.counts.resolve, 1);
+                    });
+
+                    test('reviver was called four times', function () {
+                        assert.strictEqual(log.counts.reviver, 4);
+                    });
+
+                    test('reviver was called correctly first time', function () {
+                        assert.lengthOf(log.args.reviver[0], 2);
+                        assert.strictEqual(log.args.reviver[0][0], 'bar');
+                        assert.strictEqual(log.args.reviver[0][1], 3.14159265359);
+                    });
+
+                    test('reviver was called correctly second time', function () {
+                        assert.strictEqual(log.args.reviver[1][0], 'foo');
+                        assert.isObject(log.args.reviver[1][1]);
+                        assert.lengthOf(Object.keys(log.args.reviver[1][1]), 1);
+                        assert.strictEqual(log.args.reviver[1][1].bar, 'reviver result');
+                    });
+
+                    test('reviver was called correctly third time', function () {
+                        assert.strictEqual(log.args.reviver[2][0], 'baz');
+                        assert.isNull(log.args.reviver[2][1]);
+                    });
+
+                    test('reviver was called correctly fourth time', function () {
+                        assert.strictEqual(log.args.reviver[3][0], '');
+                        assert.isObject(log.args.reviver[3][1]);
+                        assert.lengthOf(Object.keys(log.args.reviver[3][1]), 2);
+                        assert.strictEqual(log.args.reviver[3][1].foo, 'reviver result');
+                        assert.strictEqual(log.args.reviver[3][1].baz, 'reviver result');
                     });
                 });
             });
