970a9649b8bd65534003572b5412d88a53c61a13
Phil Booth <pmbooth@gmail.com>
2017-08-26 12:50:34 +0100

    refactor(walk): discard chunks more aggressively

--- src/parse.js
+++ src/parse.js
@@ -17,9 +17,6 @@ module.exports = parse
  *
  * @option reviver:   Transformation function, invoked depth-first.
  *
- * @option discard:   The number of characters to process before discarding
- *                    them to save memory. The default value is `1048576`.
- *
  * @option yieldRate: The number of data items to process per timeslice,
  *                    default is 16384.
  **/
